e75077e6b96dc729078cd297d42ae183eb5a45ea
Noah Evans <noah@itsnoahevans.co.uk>
2020-08-25 09:18:35 +0100

    remove /Users/noah/

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 # Remember, change the username to yours and change the file paths, etc. 
 
-sudo /Users/noah/iconsur set "/Applications/Visual Studio Code - Insiders.app"  -l -i "/Users/noah/Icons/vs-code.png" -s 1.25
-sudo /Users/noah/iconsur set "/Applications/Spark.app"  -l -i "/Users/noah/Icons/spark.png" -s 1.25
-sudo /Users/noah/iconsur set "/Applications/Slack.app"  -l -i "/Users/noah/Icons/slack.png" -s 1.25
-sudo /Users/noah/iconsur set "/Applications/Atom Nightly.app"  -l -i "/Users/noah/Icons/atom.icns" -s 1.25
+sudo ~/iconsur set "/Applications/Visual Studio Code - Insiders.app"  -l -i "/Users/noah/Icons/vs-code.png" -s 1.25
+sudo ~/iconsur set "/Applications/Spark.app"  -l -i "/Users/noah/Icons/spark.png" -s 1.25
+sudo ~/iconsur set "/Applications/Slack.app"  -l -i "/Users/noah/Icons/slack.png" -s 1.25
+sudo ~/iconsur set "/Applications/Atom Nightly.app"  -l -i "/Users/noah/Icons/atom.icns" -s 1.25
 
-sudo /Users/noah/iconsur cache
+sudo ~/iconsur cache
